8c90eaef6b028cac3d95473ae7029ea644e22ac8
shakhin1974 <57936556+shakhin1974@users.noreply.github.com>
2025-03-13 13:47:50 +0300

    MyForm.h

--- gra/MyForm.h
+++ gra/MyForm.h
@@ -11,7 +11,7 @@ namespace gra {
 	using namespace System::Drawing;
 
 	/// <summary>
-	/// Сводка для MyForm
+	/// РЎРІРѕРґРєР° РґР»СЏ MyForm
 	/// </summary>
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
@@ -20,13 +20,13 @@ namespace gra {
 		{
 			InitializeComponent();
 			//
-			//TODO: добавьте код конструктора
+			//TODO: РґРѕР±Р°РІСЊС‚Рµ РєРѕРґ РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂР°
 			//
 		}
 
 	protected:
 		/// <summary>
-		/// Освободить все используемые ресурсы.
+		/// РћСЃРІРѕР±РѕРґРёС‚СЊ РІСЃРµ РёСЃРїРѕР»СЊР·СѓРµРјС‹Рµ СЂРµСЃСѓСЂСЃС‹.
 		/// </summary>
 		~MyForm()
 		{
@@ -37,38 +37,43 @@ namespace gra {
 		}
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button4;
+
 	protected:
 
 	private:
 		/// <summary>
-		/// Обязательная переменная конструктора.
+		/// РћР±СЏР·Р°С‚РµР»СЊРЅР°СЏ РїРµСЂРµРјРµРЅРЅР°СЏ РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂР°.
 		/// </summary>
 		System::ComponentModel::Container ^components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
-		/// Требуемый метод для поддержки конструктора — не изменяйте 
-		/// содержимое этого метода с помощью редактора кода.
+		/// РўСЂРµР±СѓРµРјС‹Р№ РјРµС‚РѕРґ РґР»СЏ РїРѕРґРґРµСЂР¶РєРё РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂР° вЂ” РЅРµ РёР·РјРµРЅСЏР№С‚Рµ 
+		/// СЃРѕРґРµСЂР¶РёРјРѕРµ СЌС‚РѕРіРѕ РјРµС‚РѕРґР° СЃ РїРѕРјРѕС‰СЊСЋ СЂРµРґР°РєС‚РѕСЂР° РєРѕРґР°.
 		/// </summary>
 		void InitializeComponent(void)
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(916, 617);
+			this->button1->Location = System::Drawing::Point(1175, 566);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(75, 23);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"||";
+			this->button1->Text = L"/\\";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(837, 617);
+			this->button2->Location = System::Drawing::Point(1096, 566);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(75, 23);
 			this->button2->TabIndex = 1;
@@ -76,12 +81,36 @@ namespace gra {
 			this->button2->UseVisualStyleBackColor = true;
 			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
 			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(907, 566);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(75, 23);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"\\/";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(1001, 566);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(75, 23);
+			this->button4->TabIndex = 3;
+			this->button4->Text = L"<<";
+			this->button4->UseVisualStyleBackColor = true;
+			this->button4->Click += gcnew System::EventHandler(this, &MyForm::button4_Click);
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::Gray;
-			this->ClientSize = System::Drawing::Size(1014, 652);
+			this->ClientSize = System::Drawing::Size(1264, 601);
+			MyForm::Width = 1280;
+			MyForm::Height = 640;
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Name = L"MyForm";
@@ -89,39 +118,46 @@ namespace gra {
 			this->ResumeLayout(false);
 
 		}
-		int x1 = 0, x2 = 0, x3 = 50, x4 = 150;
 
+		int x1 = 600, x2 = 320, x3 = 680, x4 = 320;
 #pragma endregion
-	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-
-		Graphics^ g = Graphics::FromHwnd(this->Handle);
-		//g->DrawEllipse(gcnew Pen(Color::Yellow, 5), 110, 10, 110, 110);
-		//g->DrawEllipse(gcnew Pen(Color::BlueViolet, 3), 110, 110, 110, 110);
-		//g->FillRectangle(Brushes::Green, 95, 180, 141, 111);
-		//g->FillRectangle(Brushes::Tomato, 200, 200, 1, 1);
-		for (int x = 0; x < 1; x++) 
-		{
-			g->FillRectangle(Brushes::Gray, 0, -120 + x1, 100, 120);
-			g->FillRectangle(Brushes::White, 0, 0+ x1, 100, 40);
-			g->FillRectangle(Brushes::Blue, 0, 40+ x1, 100, 40);
-			g->FillRectangle(Brushes::Red, 0, 80+ x1, 100, 40);			
-			x1 += 120;
-
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) 
+	{ Graphics^ g = Graphics::FromHwnd(this->Handle);
+		for (size_t i = 0; i < 1; i++)
+		{ 	g->DrawLine(gcnew Pen(Color::Gray), x1, x2, x3, x4); //Р›РёРЅРёСЏ
+			x2 -= 10; x4 -= 10;
+			g->DrawLine(gcnew Pen(Color::White), x1, x2, x3, x4); //Р›РёРЅРёСЏ
 		}
-		
-
 	}
 	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
 		Graphics^ g = Graphics::FromHwnd(this->Handle);
-		for (int x = 0; x < 1; x++)
-		{
-			g->FillRectangle(Brushes::Gray, -100 + x2, 0, 100, 120);
-			g->FillRectangle(Brushes::White, 0 + x2, 0, 100, 40);
-			g->FillRectangle(Brushes::Blue, 0 + x2, 40, 100, 40);
-			g->FillRectangle(Brushes::Red, 0 + x2, 80, 100, 40);
-			x2 += 100;
-
+		for (size_t i = 0; i < 1; i++)
+		{ g->DrawLine(gcnew Pen(Color::Gray), x1, x2, x3, x4); //Р›РёРЅРёСЏ
+			x1 += 10;
+			x3 += 10;
+			g->DrawLine(gcnew Pen(Color::White), x1, x2, x3, x4); //Р›РёРЅРёСЏ
 		}
 	}
-	};
+private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+	Graphics^ g = Graphics::FromHwnd(this->Handle);
+	for (size_t i = 0; i < 1; i++)
+	{
+		g->DrawLine(gcnew Pen(Color::Gray), x1, x2, x3, x4); //Р›РёРЅРёСЏ
+		x2 += 10;
+		x4 += 10;
+		g->DrawLine(gcnew Pen(Color::White), x1, x2, x3, x4); //Р›РёРЅРёСЏ
+	}
+}
+private: System::Void button4_Click(System::Object^ sender, System::EventArgs^ e) {
+	Graphics^ g = Graphics::FromHwnd(this->Handle);
+	for (size_t i = 0; i < 1; i++)
+	{
+
+		g->DrawLine(gcnew Pen(Color::Gray), x1, x2, x3, x4); //Р›РёРЅРёСЏ
+		x1 -= 10;
+		x3 -= 10;
+		g->DrawLine(gcnew Pen(Color::White), x1, x2, x3, x4); //Р›РёРЅРёСЏ
+	}
+}
+};
 }
